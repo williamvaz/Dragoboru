939bda592cb1be506c31d3c6baf56848d36fd334
William <william.vaz.will@gmail.com>
2025-06-24 22:24:04 -0300

    Revert "Correção do chat"
    This reverts commit 991bd2517b2ae5f449bac9472af4f09d86ec72bc.

--- JS/cards.js
+++ JS/cards.js
@@ -263,13 +263,9 @@ function gerarCards() {
 
         if (filtroRaridade !== 'Todas' && carta.Raridade !== filtroRaridade) return false;
         if (filtroSaga !== 'Todas' && carta.Saga !== filtroSaga) return false;
-        if (
-    (filtroStatus === 'desbloqueados' && !desbloqueado) ||
-    (filtroStatus === 'bloqueados' && desbloqueado)
-) {
-    return false;
-}
-
+        if (filtroStatus === 'desbloqueados' && !desbloqueado) return false;
+        if (filtroStatus === 'bloqueados' && desbloqueado) return false;
+        return true;
     });
 
     lista.sort((a, b) => {
